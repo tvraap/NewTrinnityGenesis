6e5130c1bc5cc5280029416544083add9dc1e020
tvraap <tvraap18@gmail.com>
2026-05-22 12:40:37 +0200

    first push

--- KE03_INTDEV_SE_1_Base/Pages/Index.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/Index.cshtml.cs
@@ -9,20 +9,23 @@ namespace KE03_INTDEV_SE_1_Base.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ICustomerRepository _customerRepository;
-
+        private readonly IProductRepository _productRepository;
         public IList<Customer> Customers { get; set; }
+        public IList<Product> Products { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger, ICustomerRepository customerRepository)
+        public IndexModel(ILogger<IndexModel> logger, ICustomerRepository customerRepository, IProductRepository productRepository)
         {
             _logger = logger;
             _customerRepository = customerRepository;
+            _productRepository = productRepository;
             Customers = new List<Customer>();
+            Products = new List<Product>();
         }
-
         public void OnGet()
-        {            
-            Customers = _customerRepository.GetAllCustomers().ToList();                            
-            _logger.LogInformation($"getting all {Customers.Count} customers");
+        {
+            Customers = _customerRepository.GetAllCustomers().ToList();
+            Products = _productRepository.GetAllProducts().ToList();
+            _logger.LogInformation($"getting all {Customers.Count} customers and {Products.Count} products");
         }
     }
 }
---
+++ KE03_INTDEV_SE_1_Base/Pages/assortiment.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KE03_INTDEV_SE_1_Base.Pages
+{
+    public class assortimentModel : PageModel
+    {
+        private readonly ILogger<assortimentModel> _logger;
+
+        public assortimentModel(ILogger<assortimentModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
